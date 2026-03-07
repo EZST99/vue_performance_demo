d6d366287cd3fda7558b7e046911ba3af14114d4
Irem <irem.arslan@gmx.at>
2026-03-07 14:18:58 +0100

    Bugfix: Removes prefetching and fixes order

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,3 @@
-<script setup>
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  import('../views/ListView.vue')
-})
-</script>
-
 <template>
   <h1>Home</h1>
   <p>This is the home page.</p>
